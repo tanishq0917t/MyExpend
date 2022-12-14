17eb0d00d10f6ab2deb4f9a9895b06ca7fb57390
Tanishq Rawat <67191912+tanishq-17102001@users.noreply.github.com>
2022-12-14 10:39:24 +0530

    Optimization for better performance on setUp()

--- app.py
+++ app.py
@@ -18,54 +18,38 @@ def vv():
 def setUp():
     if request.method=="POST":
         a=request.form
-        if a['user'].upper()=="*******" and a['pass']=="**************":
-            with open("*******.txt","a+") as file:
-                d=str(datetime.datetime.now().day)+"-"+str(datetime.datetime.now().month)+"-"+str(datetime.datetime.now().year)
-                file.write(d)
-                file.write("\n")
-                file.write(a['amount'])
-                file.write("\n")
-                file.write(a['note'])
-                file.write("\n")
-            return "1"
-        elif a['user'].upper()=="*****" and a['pass']=="**************":
-            with open("*****.txt","a+") as file:
-                d=str(datetime.datetime.now().day)+"-"+str(datetime.datetime.now().month)+"-"+str(datetime.datetime.now().year)
-                file.write(d)
-                file.write("\n")
-                file.write(a['amount'])
-                file.write("\n")
-                file.write(a['note'])
-                file.write("\n")
-            return "1"
-        else:
-            return "0"
+
+        fname=""
+        if a['user'].upper()=="TANISHQ" and a['pass']=="Tanishq@1710h": fname="tanishq.txt"
+        elif a['user'].upper()=="HONEY" and a['pass']=="Tanishq@1710h": fname="honey.txt"
+        else: return "0"
+        with open(fname,"a+") as file:
+            d=str(datetime.datetime.now().day)+"-"+str(datetime.datetime.now().month)+"-"+str(datetime.datetime.now().year)
+            file.write(d)
+            file.write("\n")
+            file.write(a['amount'])
+            file.write("\n")
+            file.write(a['note'])
+            file.write("\n")
+        return "1"
 
 @app.route('/give',methods=['POST','GET'])
 def set():
     if request.method=="POST":
         a=request.form
-        if a['user'].upper()=="*******" and a['pass']=="**************":
-            a=open("*******.txt","r").read().strip()
-            a=a.split("\n")
-            print(a)
-            aa=""
-            for i in a:
-                aa+=i
-                aa+=","
-            aa=aa[0:-1]
-            return aa
-        elif a['user'].upper()=="*****" and a['pass']=="**************":
-            a=open("*****.txt","r").read().strip()
-            a=a.split("\n")
-            print(a)
-            aa=""
-            for i in a:
-                aa+=i
-                aa+=","
-            aa=aa[0:-1]
-            return aa
-        else:
-            return "0"
+        fname=""
+        if a['user'].upper()=="TANISHQ" and a['pass']=="Tanishq@1710h": fname="tanishq.txt"
+        elif a['user'].upper()=="HONEY" and a['pass']=="Tanishq@1710h": fname="honey.txt"
+        else: return "0"
+        a=open(fname,"r").read().strip()
+        a=a.split("\n")
+        print(a)
+        aa=""
+        for i in a:
+            aa+=i
+            aa+=","
+        aa=aa[0:-1]
+        return aa
+        
 if __name__ == '__main__':
     app.run(debug = True)
